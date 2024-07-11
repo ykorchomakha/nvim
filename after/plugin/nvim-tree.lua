d9f96cbabffe2f2f0c58434cc07b4513a82926b8
Yurii Korchomakha <ykorchomakha@gmail.com>
2024-07-11 21:33:14 +0300

    use full screen for nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -4,21 +4,31 @@ vim.g.loaded_netrwPlugin = 1
 
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
-vim.g.nvim_tree_auto_open=1
+vim.g.nvim_tree_auto_open = 1
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    actions = {
+        open_file = {
+            quit_on_open = true,
+        },
+    },
+    renderer = {
+        group_empty = true,
+    },
+})
+
+local api = require("nvim-tree.api")
 
 -- automatically open file upon creation
-local api = require("nvim-tree.api")
 api.events.subscribe(api.events.Event.FileCreated, function(file)
     vim.cmd("edit " .. file.fname)
 end)
 
-local nvimTreeFocusOrToggle = function()
-    local nvimTree = require("nvim-tree.api")
-    nvimTree.tree.toggle()
+local function show_tree()
+    if not api.tree.is_visible() then
+        api.tree.open({ current_window = true })
+    end
 end
 
-vim.keymap.set("n", "<leader>pv", nvimTreeFocusOrToggle)
-
+vim.keymap.set("n", "<leader>pv", show_tree)
