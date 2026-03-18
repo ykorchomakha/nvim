64eb5fde54ce842be428cb2730c098210e817390
Yurii Korchomakha <ykorchomakha@gmail.com>
2026-03-18 14:38:25 +0200

    inline vim config

--- lua/conf/set.lua
+++ lua/conf/set.lua
@@ -1,5 +1,51 @@
-vim.cmd("source ~/.vimrc")
+-- Enable syntax highlighting
+vim.cmd("syntax on")
 
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Tabs & indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+
+-- Display
+vim.opt.wrap = false
+vim.opt.colorcolumn = "100"
+
+-- Search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- List chars
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "›⋅",
+  trail = "⋅",
+  space = "⋅",
+}
+
+-- Cursor line
+vim.opt.cursorline = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Files
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+-- Scrolling & UI
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+-- Colors
+vim.opt.termguicolors = true
 vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
 
 -- stop extending comments automatically
